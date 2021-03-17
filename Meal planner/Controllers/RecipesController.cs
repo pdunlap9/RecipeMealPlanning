435aee9568ba2e59e3c151b10d5ab587a292c96c
pdunlap <xtrishayy@yahoo.com>
2021-03-17 18:43:56 -0500

    Beginning

---
+++ Meal planner/Controllers/RecipesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meal_planner.Controllers
+{
+    public class RecipesController : Controller
+    {
+        static private List<string> Recipes = new List<string>();
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Recipes.Add("Chicken Carbonara");
+            Recipes.Add("Macaroni and Cheese");
+            Recipes.Add("Yellow Chicken Curry");
+            ViewBag.recipes = Recipes;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
