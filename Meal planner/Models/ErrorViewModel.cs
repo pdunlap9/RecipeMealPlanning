046ed6e7b7198ad104f893739fd4885214315fa9
pdunlap <xtrishayy@yahoo.com>
2021-03-17 17:45:14 -0500

    Add project files.

---
+++ Meal planner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Meal_planner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
