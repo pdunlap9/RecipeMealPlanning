40ebee14a84903121f0253f030697be415e18c40
pdunlap <xtrishayy@yahoo.com>
2021-03-17 17:58:33 -0500

    First

---
+++ Meal planner/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meal_planner.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET:/<controller>/
+        public IActionResult Index()
+        {
+            return Content("Hello  World");
+        }
+    }
+}
